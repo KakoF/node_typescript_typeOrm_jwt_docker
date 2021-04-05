65ee5a35ea5dd51e123b531071b839ba44948f1f
Marcos <kakoferrare87@gmail.com>
2021-04-05 17:04:38 -0300

    Exemplo de inicio da implementação do controller

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -24,3 +24,30 @@ class UserController {
 }
 
 export default new UserController()
+
+/*import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import { User } from '../../domain/entities/User'
+import { CreateUserUseCase } from '../../useCases/User/CreateUserUseCase'
+class UserController {
+  private _createUseCase: CreateUserUseCase
+  constructor(createUseCase: CreateUserUseCase) {
+    this._createUseCase = createUseCase
+  }
+  async store(req: Request, res: Response) {
+   
+    const { email, password } = req.body
+    const user = this._createUseCase.execute({ email, password })
+    return res.json(user)
+  }
+  async index(req: Request, res: Response) {
+    const repository = getRepository(User)
+    const users = await repository.find()
+    return res.json({ ID: req.userId, users: users })
+  }
+  async init(req: Request, res: Response) {
+    return res.send('ok')
+  }
+}
+
+export default new UserController()*/
